61ee62bfd911a10aa49ac125013bc4d02da10245
Guhszvv <vruum962@gmail.com>
2026-07-31 16:40:04 -0300

    feat: Add update checker infrastructure

--- src/github/update_checker.rs
+++ src/github/update_checker.rs
@@ -9,15 +9,13 @@ pub async fn update_checker() -> Result<(bool, String), Box<dyn Error>> {
         tag_name: String,
     }
 
-    let current = semver::Version::parse("0.1.0")?;
-    /*let resp = reqwest::get("https://api.github.com/repos/Guhszvv/lazyact/releases/latest")
+    let current = semver::Version::parse(env!("CARGO_PKG_VERSION"))?;
+    let resp = reqwest::get("https://api.github.com/repos/Guhszvv/lazyact/releases/latest")
         .await?
         .json::<Response>()
         .await?;
-    */
-    let test_version = "9999.0.0".to_string();
-    let latest = semver::Version::parse(test_version.as_str())?;
+    let latest = semver::Version::parse(resp.tag_name.as_str())?;
     let has_update = latest > current;
 
-    Ok((has_update, test_version))
+    Ok((has_update, latest.to_string()))
 }
